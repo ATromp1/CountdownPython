8aa969ab14debd1ce722a4859968a455972325ca
Alexander Tromp <alexandert@live.nl>
2019-10-06 00:02:13 +0200

    finished up analysis result

--- analyse_result.py
+++ analyse_result.py
@@ -1,13 +1,22 @@
+import matplotlib.pyplot as plt
+
 def main():
     f = open("result.txt", "r")
     f1 = f.readlines()
-    iteration = []
-    amount = []
+
+    max = 0
+    numberlist = []
     for x in f1:
-        result = x.split()
-        #print(result)
-        iteration.append(result[0])
-        amount.append(int(result[1]))
+        num = int(x.split("]", 1)[1])
+        numberlist.append(num)
+        if num > max:
+            max = num
+
+    print("Max attainable values between 101-999: ", max)
+    plt.plot(numberlist, linewidth=0.4)
+    plt.ylabel('Possible answers')
+    plt.xlabel('Card combinations')
+    plt.show()
     f.close()
 
 
